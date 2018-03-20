cb58d5045d2618c64f65ee978f6d872152a8dcf3
sebamalisz <malisz654321@gmail.com>
2018-03-20 09:21:56 +0100

    Little code cleanup

--- scripts/odbior.js
+++ scripts/odbior.js
@@ -5,8 +5,6 @@ function odbierz(e){
     let czasOD =  document.querySelector('#timeFrom').value;
     let czasDO =  document.querySelector('#timeTo').value;
     let opisWyd =  document.querySelector('#opisWyd').value;
-    // console.log("odebrano"+" "+nazwaWyd+" "+czasOD+" "+czasDO+" "+opisWyd+" "+nazwyMiesiecy[miesiac]+" "+kliknietyDzien+" "+rok);
-    // tworzenie obiektu przetrzymujacego dane
     let wydarzenieObiekt = {
         nazwaWydarzenia : nazwaWyd, 
         odKiedy : czasOD, 
@@ -18,8 +16,8 @@ function odbierz(e){
         rokWydarzenia : rok
     };
     console.log(wydarzenieObiekt);
+    
     //ustawianie w storage danych (musze do stringa przerobic obiekt, zeby go odpowiednio umiescic w storage)
     localStorage.setItem(timeStamp, JSON.stringify(wydarzenieObiekt));
     
-    // return [nazwaWyd, czasOD, czasDO, opisWyd];
 }
--- scripts/script.js
+++ scripts/script.js
@@ -3,76 +3,81 @@
  // zmienne do nastepnego i poprzednego  miesiaca
  let nextMonth = 0;
  // zmienne do rysowania obecnego miesiaca
-var obecnyCzas = new Date();
-var timeStamp = Date.now();
+ var obecnyCzas = new Date();
+ var timeStamp = Date.now();
  let dzis = obecnyCzas.getDate();
  let rok = obecnyCzas.getFullYear();
  let miesiac = obecnyCzas.getMonth();
  let dzienTygodnia = obecnyCzas.getDay(); // 1 - poniedzialek
  // day - zmienna sprawdzajaca jaki dzien tygodnia jest pierwszym dniem w miesiacu
  let day = new Date(rok, miesiac, 1).getDay();
+
  function leapYear(year) {
      return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
  }
-const nazwyMiesiecy = ["Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"];
+ const nazwyMiesiecy = ["Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"];
  const dniTygodniaNazwa = ["", "Pon.", "Wt.", "Śr.", "Czw.", "Pt.", "Sob.", "Niedz."];
  const dniMiesiecyZwykly = [31, // styczen- miesdiac nr 0 dniMiesiecyZwykly[0]
-     29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+     29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+ ];
  const dniMiesiecyPrzestepny = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
  //RYSOWANIE MIESIACA
- function drawMonth(nazwaMiesiaca, dzienTygodnia, dniTygodniaNazwa, day, rok, dniMiesiecyZwykly, nextMonth) 
- {
-    clear();
+ function drawMonth(nazwaMiesiaca, dzienTygodnia, dniTygodniaNazwa, day, rok, dniMiesiecyZwykly, nextMonth) {
+     clear();
      document.querySelector(".nazwa-miesiaca").innerHTML = nazwaMiesiaca[miesiac] + ", " + rok;
-     if (day === 0){ day = 7 };
-     for (j = 0; j < 7; j++){
-             document.querySelector('.test').innerHTML += '<div class="tyg nazwa-dnia' + (j + 1) + '">' + dniTygodniaNazwa[j + 1] + '</div>';}
-     for (let i = 1; i < dniMiesiecyZwykly[miesiac] + day; i++){
-         if (day >= i){                    
+     if (day === 0) {
+         day = 7
+     };
+     for (j = 0; j < 7; j++) {
+         document.querySelector('.test').innerHTML += '<div class="tyg nazwa-dnia' + (j + 1) + '">' + dniTygodniaNazwa[j + 1] + '</div>';
+     }
+     for (let i = 1; i < dniMiesiecyZwykly[miesiac] + day; i++) {
+         if (day >= i) {
              $('.box').css('opacity', '0')
-             }
-         if(i === dzis && miesiac == obecnyCzas.getMonth()){document.querySelector('.test').innerHTML += '<div id="day_' + (i - day + 1) + '" class="box day' + (i - day + 1) + ' ' + nazwyMiesiecy[miesiac + nextMonth] + (i - day + 1) + rok +" " + (i - day + 1)+" "+"today"+ ' ">' + (i - day + 1) + '</div>';}   else{
-         document.querySelector('.test').innerHTML += '<div id="day_' + (i - day + 1) + '" class="box day' + (i - day + 1) + ' ' + nazwyMiesiecy[miesiac + nextMonth] + (i - day + 1) + rok +" " + (i - day + 1)+ ' ">' + (i - day + 1) + '</div>';}
          }
-    hasEvent();
+         if (i === dzis && miesiac == obecnyCzas.getMonth()) {
+             document.querySelector('.test').innerHTML += '<div id="day_' + (i - day + 1) + '" class="box day' + (i - day + 1) + ' ' + nazwyMiesiecy[miesiac + nextMonth] + (i - day + 1) + rok + " " + (i - day + 1) + " " + "today" + ' ">' + (i - day + 1) + '</div>';
+         } else {
+             document.querySelector('.test').innerHTML += '<div id="day_' + (i - day + 1) + '" class="box day' + (i - day + 1) + ' ' + nazwyMiesiecy[miesiac + nextMonth] + (i - day + 1) + rok + " " + (i - day + 1) + ' ">' + (i - day + 1) + '</div>';
+         }
+     }
+     hasEvent();
  }
  const chwyt = document.getElementsByClassName('box');
- 
- function clear()
- {
+
+ function clear() {
      document.querySelector(".test").innerHTML = "";
  }
 
-// SPRAWDZANIE JAKIE DNI MAJA EVENTY I WRZUCANIE IM KLASY KTORA DODA OZNACZENIE
-function hasEvent(){
- let eventDay = [];
- let days;
- 
- let objectKey = [];
- const childDiv = document.createElement('div');
- for (i = 0; i < localStorage.length; i++) {
-     key = localStorage.key(i);
-     objectKey[i] = JSON.parse(localStorage.getItem(key));
-     
-     if(objectKey[i].miesiacWydarzeniaLiczba == miesiac && objectKey[i].rokWydarzenia == rok ){
-         eventDay[i] = objectKey[i].dzienWydarzenia;
-        NodeList.prototype.forEach = Array.prototype.forEach
-         var children = document.querySelector('.test').childNodes;
-         children.forEach(function(item){
-           if(parseInt(item.classList.item(3)) == eventDay[i]){
-                 item.innerHTML+= `
+ // SPRAWDZANIE JAKIE DNI MAJA EVENTY I WRZUCANIE IM KLASY KTORA DODA OZNACZENIE
+ function hasEvent() {
+     let eventDay = [];
+     let days;
+
+     let objectKey = [];
+     const childDiv = document.createElement('div');
+     for (i = 0; i < localStorage.length; i++) {
+         key = localStorage.key(i);
+         objectKey[i] = JSON.parse(localStorage.getItem(key));
+
+         if (objectKey[i].miesiacWydarzeniaLiczba == miesiac && objectKey[i].rokWydarzenia == rok) {
+             eventDay[i] = objectKey[i].dzienWydarzenia;
+             NodeList.prototype.forEach = Array.prototype.forEach
+             var children = document.querySelector('.test').childNodes;
+             children.forEach(function (item) {
+                 if (parseInt(item.classList.item(3)) == eventDay[i]) {
+                     item.innerHTML += `
                     <div class='has-Event'></div>  
                      `
-             }
-        });
-         
-     }
-    }
-}
+                 }
+             });
 
- function startTime() 
- {
+         }
+     }
+ }
+
+ function startTime() {
      let dzis = new Date();
      let h = dzis.getHours();
      let m = dzis.getMinutes();
@@ -83,49 +88,53 @@ function hasEvent(){
          h + " : " + m + " : " + '<div class="sec">' + s + '</div>';
      let t = setTimeout(startTime, 500);
  }
- function checkTime(i) 
- {
-     if (i < 10) { i = "0" + i };  // dodaj zero jesli numer < 10
+
+ function checkTime(i) {
+     if (i < 10) {
+         i = "0" + i
+     }; // dodaj zero jesli numer < 10
      return i;
  }
- 
- window.onload = function () 
- {
+
+ window.onload = function () {
      drawMonth(nazwyMiesiecy, dzienTygodnia, dniTygodniaNazwa, day, rok, dniMiesiecyZwykly, nextMonth);
      startTime();
-     
-     
+
+
  };
  // Programowanie klikow na next miesiac i poprzedni miesiac
-     let guzik = document.querySelector('.nextMonthButt');
-     
-     guzik.onclick = function (e){
-        miesiac++;
-        day = new Date(rok, miesiac, 1).getDay();
-        if (day === 0) { day = 7 };
-        if (e.preventDefault) {
-            e.preventDefault();
+ let guzik = document.querySelector('.nextMonthButt');
+
+ guzik.onclick = function (e) {
+     miesiac++;
+     day = new Date(rok, miesiac, 1).getDay();
+     if (day === 0) {
+         day = 7
+     };
+     if (e.preventDefault) {
+         e.preventDefault();
      }
      e.returnValue = false;
-     if(miesiac %12 == 0 ){
+     if (miesiac % 12 == 0) {
          miesiac = 0;
          rok++;
          console.log("pyrklo");
      }
      drawMonth(nazwyMiesiecy, dzienTygodnia, dniTygodniaNazwa, day, rok, dniMiesiecyZwykly, nextMonth);
  };
- 
+
  // poprzedni miesiac
-     let guzik2 = document.querySelector('.prevMonthButt');
-     guzik2.onclick = function (e) 
- {
+ let guzik2 = document.querySelector('.prevMonthButt');
+ guzik2.onclick = function (e) {
      miesiac--;
      day = new Date(rok, miesiac, 1).getDay();
-     if (day === 0) { day = 7 };
+     if (day === 0) {
+         day = 7
+     };
      if (e.preventDefault) {
          e.preventDefault();
      }
-     if(miesiac <= 0){
+     if (miesiac <= 0) {
          miesiac = 12;
          rok--;
      }
@@ -137,25 +146,25 @@ function hasEvent(){
  $('.test').on('click', '.box', pokazOkno);
  $('.test').on('click', '.box', getLocalStorage);
 
- function pokazOkno(){
+ function pokazOkno() {
      // wyciagniecie ktory dzien zostal klikniety
      let kliknietyDzienID = this.id;
-     const dniMies =[];
-     for(let i =1;i<=31;i++){
-         dniMies[i] = ["day_"+i];
+     const dniMies = [];
+     for (let i = 1; i <= 31; i++) {
+         dniMies[i] = ["day_" + i];
      };
      let jakidzien = "";
-     for (let i = 0; i < dniMies.length; i++) 
-     {
+     for (let i = 0; i < dniMies.length; i++) {
          if (dniMies[i] == kliknietyDzienID)
              jakidzien = i;
      }
      // przypisanie dnia do globalnej zmiennej
      kliknietyDzien = jakidzien;
+
      // wsadzenie forma w diva
      let formHTML = `
      <form id="calAdd" action="" method="post">
-     <p>Dodaj akcję do kalendarza na dzien <span class="info-callAdd">`+ jakidzien + ' ' + nazwyMiesiecy[miesiac] + `</span></p>
+     <p>Dodaj akcję do kalendarza na dzien <span class="info-callAdd">` + jakidzien + ' ' + nazwyMiesiecy[miesiac] + `</span></p>
      <fieldset>
        <input id="nazWyd" placeholder="Nazwa wydarzenia" type="text" tabindex="1" required autofocus>
      </fieldset>
@@ -179,37 +188,39 @@ function hasEvent(){
                  `;
      document.querySelector('.addEvent').classList.add("pojaw");
      document.querySelector('.addEvent').innerHTML = formHTML;
-    return kliknietyDzien;
-}
-// obslugiwanie local storage
+     return kliknietyDzien;
+ }
+ // obslugiwanie local storage
 
-if (typeof(Storage)!=="undefined") {
-// Web Storage dostępny
+ if (typeof (Storage) !== "undefined") {
+     // Web Storage dostępny
 
-} else {
-// Web Storage nie wspierany
-console.log("niedostepny");
-}
-//RYSOWANIE ZRZUCONYCH W LOCAL STORAGE DANYCH NA APLIKACJE (HTML)   
-    //proba wyciagniecia z local storage itemu oraz sparsowania go do obiektu spowrotem
-function getLocalStorage(){
-function clearPlace(){
-    miejsce.innerHTML="";
-    }
-let i,ls = localStorage,l = ls.length,key,objectKey = [];
-const miejsce = document.querySelector('.zaplanowane-content');
-clearPlace();
- let info = [];
-// petla przelatuje cale localstorage, key lapie nazwy wydarzen
-for (i = 0; i < l; i++) {
-key = ls.key(i);
-objectKey[i] = JSON.parse(ls.getItem(key));
+ } else {
+     // Web Storage nie wspierany
+     console.log("niedostepny");
+ }
+ //RYSOWANIE ZRZUCONYCH W LOCAL STORAGE DANYCH NA APLIKACJE (HTML)   
+ //proba wyciagniecia z local storage itemu oraz sparsowania go do obiektu spowrotem
+ function getLocalStorage() {
+     function clearPlace() {
+         miejsce.innerHTML = "";
+     }
+     let i, ls = localStorage,
+         l = ls.length,
+         key, objectKey = [];
+     const miejsce = document.querySelector('.zaplanowane-content');
+     clearPlace();
+     let info = [];
+     // petla przelatuje cale localstorage, key lapie nazwy wydarzen
+     for (i = 0; i < l; i++) {
+         key = ls.key(i);
+         objectKey[i] = JSON.parse(ls.getItem(key));
 
-//sprawdzam czy klikniety dzien ma odpowiedni zapis w storage
-if(objectKey[i].dzienWydarzenia === kliknietyDzien && objectKey[i].miesiacWydarzeniaLiczba == miesiac && objectKey[i].rokWydarzenia == rok ){
-    info[i] = key;
-    miejsce.innerHTML += `
-    <div class="Planned-Events-Info" id="`+key+`" >
+         //sprawdzam czy klikniety dzien ma odpowiedni zapis w storage
+         if (objectKey[i].dzienWydarzenia === kliknietyDzien && objectKey[i].miesiacWydarzeniaLiczba == miesiac && objectKey[i].rokWydarzenia == rok) {
+             info[i] = key;
+             miejsce.innerHTML += `
+    <div class="Planned-Events-Info" id="` + key + `" >
     <div class="exit-Box" title="Usunięcie wpisu z kalendarza"></div>
     <div class="eventDivKeeper"> 
         <span class="eventSpan-Name">
@@ -228,37 +239,35 @@ if(objectKey[i].dzienWydarzenia === kliknietyDzien && objectKey[i].miesiacWydarz
     </div>
     <div class="eventDivKeeper2"> 
             <span class="eventSpan-Name-js">
-                    `+objectKey[i].nazwaWydarzenia+`
+                    ` + objectKey[i].nazwaWydarzenia + `
             </span>
             
             <span class="eventSpan-FromTime-js">
-                    `+objectKey[i].odKiedy+`
+                    ` + objectKey[i].odKiedy + `
             </span>
             <span class="eventSpan-ToTime-js">
-                    `+objectKey[i].doKiedy+`
+                    ` + objectKey[i].doKiedy + `
             </span>
             <span class="eventSpan-Desc-js">
-                    `+objectKey[i].opisWydarzenia+` 
+                    ` + objectKey[i].opisWydarzenia + ` 
             </span>
         </div>
     </div>`;
- $(".Planned-Events-Info").addClass('pojaw');
- 
-}
-}
-// USUWANIE  WYBRANEGO ELEMENTU Z LOCAL STORAGE 
+             $(".Planned-Events-Info").addClass('pojaw');
 
-$('.zaplanowane-content').on('click','.exit-Box', function(){
-if (confirm('Czy jesteś pewny, że chcesz usunąć wpis z kalendarza?')) {
- localStorage.removeItem(this.parentElement.id);
- window.location.reload();
- hasEvent();
- 
-} else {
-return;
-}
-});
+         }
+     }
+     // USUWANIE  WYBRANEGO ELEMENTU Z LOCAL STORAGE 
 
-}
+     $('.zaplanowane-content').on('click', '.exit-Box', function () {
+         if (confirm('Czy jesteś pewny, że chcesz usunąć wpis z kalendarza?')) {
+             localStorage.removeItem(this.parentElement.id);
+             window.location.reload();
+             hasEvent();
 
+         } else {
+             return;
+         }
+     });
 
+ }
